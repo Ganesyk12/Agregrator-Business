e6b6d1a4075d8444c27841dbd3057f51c99991a2
Ganesyk <ganesyudhakusuma@gmail.com>
2026-07-01 00:21:55 +0700

    feat: add environment configuration and initial App component with dynamic title rendering

--- src/origin/App.vue
+++ src/origin/App.vue
@@ -1,11 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const title = import.meta.env.VITE_APP_TITLE
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>{{ title }}</h1>
 </template>
 
 <style scoped></style>
